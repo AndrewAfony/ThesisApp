785f784ddc28caf860721f8efec16f96575668f8
Andrew <andrew.afony@yandex.ru>
2023-04-24 13:15:24 +0300

    add deadlines check vibration

--- app/src/main/java/andrewafony/thesis/application/feature_deadlines/presentation/FragmentDeadlines.kt
+++ app/src/main/java/andrewafony/thesis/application/feature_deadlines/presentation/FragmentDeadlines.kt
@@ -3,12 +3,16 @@ package andrewafony.thesis.application.feature_deadlines.presentation
 import andrewafony.thesis.application.R
 import andrewafony.thesis.application.ViewModelFactoryProvider
 import andrewafony.thesis.application.core.BaseFragment
+import andrewafony.thesis.application.core.Vibration
 import andrewafony.thesis.application.databinding.FragmentDeadlinesBinding
 import andrewafony.thesis.application.feature_deadlines.presentation.adapter.DeadlinesAdapter
 import andrewafony.thesis.application.feature_deadlines.presentation.adapter.DeadlinesViewHolderFabric
-import android.os.Bundle
+import android.content.Context
+import android.os.*
 import android.view.LayoutInflater
 import android.view.View
+import androidx.annotation.RequiresApi
+import androidx.core.content.getSystemService
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -17,15 +21,20 @@ class FragmentDeadlines: BaseFragment<FragmentDeadlinesBinding>() {
 
     private val viewModel by activityViewModels<DeadlinesViewModel>(factoryProducer = { (activity as ViewModelFactoryProvider).provide() })
 
+    private lateinit var vibrator: Vibration
+
     override val bindingInflater: (LayoutInflater) -> FragmentDeadlinesBinding
         get() = FragmentDeadlinesBinding::inflate
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        vibrator = Vibration.Base(requireContext())
+
         val deadlinesAdapter = DeadlinesAdapter(DeadlinesViewHolderFabric(
             updateDoneState = { item ->
                 viewModel.updateDoneState(item)
+                vibrator.buttonClick()
             }
         ))
 
